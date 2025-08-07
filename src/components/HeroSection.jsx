45a481c07e2c404693f6f52cfadde2cd9c332372
Saumya khushlani <saumyakhushlani9@gmail.com>
2025-08-07 18:44:03 +0530

    Desktop View

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -41,7 +41,7 @@ function HeroSection() {
 
   return (
     <div
-      className="relative pt-16 min-h-screen w-full bg-no-repeat bg-cover bg-center flex flex-col lg:flex-row text-white overflow-x-hidden"
+      className="relative py-36 md:max-h-max w-full bg-no-repeat bg-cover bg-center flex flex-col lg:flex-row text-white overflow-x-hidden"
       style={{ backgroundImage: `url(${web_gradient})` }}
     >
       {/* Radial glow background effect */}
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(), react()],
+  server: {
+    host: true, // ← crucial!
+    port: 5173, // optional
+  },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   }
