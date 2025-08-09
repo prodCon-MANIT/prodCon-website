b57ac9c15eb0562c471927075aeb5da7f9e7e1b7
Nimish Agarwal <nimishagrawal627@gmail.com>
2025-08-09 22:07:55 +0530

    Loader add

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar'
+import { useState, useEffect } from 'react';
+import NavBar from './components/NavBar';
 import About from './components/About.tsx';
 import Contact from './components/Contact';
 import Home from './components/Home';
+import { LoaderTwo } from "@/components/loader"; 
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    // 3 sec ke liye loader
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <LoaderTwo />;
+  }
 
   return (
-    < >
+    <>
       <NavBar />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
